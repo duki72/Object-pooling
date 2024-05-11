5dce588466695ab53717ea68486ef375fd839bc5
skadu07 <72766124+duki72@users.noreply.github.com>
2024-05-11 20:54:39 +0200

    Add files via upload

---
+++ PoolObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoolObject : MonoBehaviour {
+
+	//generic 
+	public virtual void OnObjectReuse() {
+
+	}
+
+	protected void Destroy() {
+		gameObject.SetActive (false);
+	}
+}
